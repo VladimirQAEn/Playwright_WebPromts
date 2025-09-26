0e83d64777a74440bb20f36b4c22ea422c49c48a
VladimirQAEn <qwer9889@proton.me>
2025-09-26 13:47:29 +0300

    test_changes_windows

--- tests/Example/05_mouthActions.spec.ts
+++ tests/Example/05_mouthActions.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('Mouth Actions', async ({ page }) => {
     //go to url
-    await page.goto('https://dou.ua/');
+    await page.goto('https://dou.ua');
 
     //mouth actions left click
     await page.getByRole('link', { name: 'Форум' }).first().click({ button: 'left'});
